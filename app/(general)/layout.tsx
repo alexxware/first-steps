3bb59305281b4232432c88b71b37900d58715fbe
alexxware <manuelmorm1@gmail.com>
2026-05-06 19:07:35 -0600

    navbar version one implemented

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,12 +1,16 @@
+import { Navbar } from '@/components';
 export default function GeneralLayout({
     children
 }: {
     children: React.ReactNode
 }) {
     return (
-        <main className="flex flex-col items-center p-24">
-            <span className="text-lg">Te damos la bienvenida a</span>
-            {children}
-        </main>
+        <>
+            <Navbar />
+            <main className="flex flex-col items-center p-24">
+                <span className="text-lg">Te damos la bienvenida a</span>
+                {children}
+            </main>
+        </>
     )
 }
